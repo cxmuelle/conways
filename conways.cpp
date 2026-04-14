0219a2667b36301205c4184e029acb03ad62bca8
cxmuelle <cxmuelle@umich.edu>
2026-04-13 21:18:53 -0500

    Worked on the skeleton of the logic for Update, Completed display, worked on the constructor assuming i'll be using bools to represent the cells ATM

--- conways.cpp
+++ conways.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 class cell{
@@ -23,18 +24,62 @@ class cell{
 class board{
     private:
     int dimension;
-    bool **field; //eventually implement a dynamic array but for the momment lets just focus on implementing the basics
+    bool* field; //eventually implement a dynamic array but for the momment lets just focus on implementing the basics
     
     public:
     board(int d){
         dimension = d;
+        field = new bool[d*d];
         // Create the field (eventually this should be read in from a file)
+        // A current work around
+        for(int x=0; x<dimension; ++x){
+            for(int y=0; y<dimension; ++y){
+                field[x*dimension+y] = bool(rand()%2);
+            }
+        }
     };
+
+    ~board(){ //whenever new, delete what was called
+        delete field;
+    };
+
     void display(){
         //2d for loops that prints the board state
+        for(int x=0; x<dimension; ++x){
+            for(int y=0; y<dimension; ++y){
+                cout<<field[x*dimension+y]<<" ";
+            }
+            cout<<endl;
+        }
     };
     void update(){
         //2d for loop that checks enforces the rules
+        int numAlive = 0;
+        for(int x=0; x<dimension; ++x){
+            for(int y=0; y<dimension; ++y){
+                if(x!=0){}
+                if(x!=dimension-1){}
+                if(y!=0){}
+                if(y!=dimension-1){}
+                if(x!=0 && y!=0){}
+                if(x!=0 && y!=dimension-1){}
+                if(x!=dimension-1 && y!=0){}
+                if(x!=dimension-1 && y!=dimension-1){}
+
+                if(field[x*dimension+y]){
+                    if(numAlive < 2 || numAlive > 3){
+                        //Eventually it will be cell.Update()
+                        field[x*dimension+y]=!field[x*dimension+y];
+                    }
+                }
+                else{
+                    if(numAlive == 3){
+                        field[x*dimension+y]=!field[x*dimension+y];
+                    }
+                }
+                numAlive=0;
+            }
+        }
     };
 };
 
@@ -44,5 +89,7 @@ int main(){
     a.display();
     a.update();
     a.display();
+    board b(10);
+    b.display();
     return 0;
 }
