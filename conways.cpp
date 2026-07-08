5e2c9752f65fa541c448e96dd019294e9a35c420
cxmuelle <cxmuelle@umich.edu>
2026-07-07 21:02:12 -0500

    Changed from using a 1d array to a 2d vector (makes it easier to parse), Inplemented the bones of the type var for the cell class. Eventually that will become the type of the vector.

--- conways.cpp
+++ conways.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
+#include <vector>
 #include <cstdlib>
 using namespace std;
+
 
 class cell{
     private:
     bool alive;
+    string type; //type is added for when there are various types of cells (wall, zombie, etc)
 
     public:
     cell(){
         alive = false;
+        type = 'n'; // n will stand for normal
     };
     cell(bool a){
         alive = a;
+        type = 'n'; // n will stand for normal
     };
+    cell(bool a, string s){
+        alive = a;
+        type = s;
+    };
+    
+    string getType(){
+        return this->type;
+    };
+
     void update(){
         alive = !alive;
     };
@@ -24,41 +38,41 @@ class cell{
 class board{
     private:
     int dimension;
-    bool* field; //eventually implement a dynamic array but for the momment lets just focus on implementing the basics
+    vector<vector<bool>> field; 
     
     public:
     board(int d){
         dimension = d;
-        field = new bool[d*d];
+        field = vector<vector<bool>>(d,vector<bool>(d));
         // Create the field (eventually this should be read in from a file)
         // A current work around
         for(int x=0; x<dimension; ++x){
             for(int y=0; y<dimension; ++y){
-                field[x*dimension+y] = bool(rand()%2);
+                field[x][y] = bool(rand()%2);
             }
         }
     };
 
     ~board(){ //whenever new, delete what was called
-        delete field;
     };
 
     void display(){
         //2d for loops that prints the board state
         for(int x=0; x<dimension; ++x){
             for(int y=0; y<dimension; ++y){
-                cout<<field[x*dimension+y]<<" ";
+                cout<<field[x][y]<<" ";
             }
             cout<<endl;
         }
     };
 
     void update(){        
-        // With the current method I NEED TO DEEP COPY (Function is still O(N^2))
-        bool* backupField = new bool[dimension*dimension];
+        // With the current method I NEED TO DEEP COPY (Function is still O(cN^2)) 
+        // c = constant
+        vector<vector<bool>>backupField(this->dimension,vector<bool>(this->dimension));
         for(int x=0; x<dimension; ++x){
             for(int y=0; y<dimension; ++y){
-                backupField[x*dimension+y] = field[x*dimension+y];
+                backupField[x][y] = field[x][y];
             }
         }
         int numAlive = 0;
@@ -66,52 +80,42 @@ class board{
             for(int y=0; y<dimension; ++y){
                 // Simple Check, North South West East
                 // I have each if make sure that we won't go over bounds by placing the in the if before checking if alive 
-                if(x!=0 && field[(x-1)*dimension+y]){
+                if(x!=0 && field[x-1][y]){
                     numAlive++;
                 }
-                if(x!=dimension-1 && field[(x+1)*dimension+y]){
+                if(x!=dimension-1 && field[(x+1)][y]){
                     numAlive++;
                 }
-                if(y!=0 && field[x*dimension+(y-1)]){
+                if(y!=0 && field[x][(y-1)]){
                     numAlive++;
                 }
-                if(y!=dimension-1 && field[x*dimension+(y+1)]){
+                if(y!=dimension-1 && field[x][(y+1)]){
                     numAlive++;
                 }
                 // Diagonal check, NW NE SW SE
-                if(x!=0 && y!=0 && field[(x-1)*dimension+(y-1)]){
+                if(x!=0 && y!=0 && field[(x-1)][(y-1)]){
                     numAlive++;
                 }
-                if(x!=0 && y!=dimension-1 && field[(x-1)*dimension+(y+1)]){
+                if(x!=0 && y!=dimension-1 && field[(x-1)][(y+1)]){
                     numAlive++;
                 }
-                if(x!=dimension-1 && y!=0 && field[(x+1)*dimension+(y-1)]){
+                if(x!=dimension-1 && y!=0 && field[(x+1)][(y-1)]){
                     numAlive++;
                 }
-                if(x!=dimension-1 && y!=dimension-1 && field[(x+1)*dimension+(y+1)]){
+                if(x!=dimension-1 && y!=dimension-1 && field[(x+1)][(y+1)]){
                     numAlive++;
                 }
-                // NEED TO MAKE A COPY OF FIELD SO CHANGES DON'T IMPACT DOWN THE LINE
-                if(field[x*dimension+y] && (numAlive < 2 || numAlive > 3)){
+                if(field[x][y] && (numAlive < 2 || numAlive > 3)){
                     //Eventually it will be cell.Update()
-                    backupField[x*dimension+y]=!field[x*dimension+y];
+                    backupField[x][y]=!field[x][y];
                     }
-                if(!field[x*dimension+y] && (numAlive == 3)){
-                    backupField[x*dimension+y]=!field[x*dimension+y];
+                if(!field[x][y] && (numAlive == 3)){
+                    backupField[x][y]=!field[x][y];
                 }
                 numAlive=0;
             }
         }
-        // I wanted to get rid of the pointer so I am ok with taking more time
-        // I am currently working with small fields so it might be an issue later
-        // Why am I doing this, I don't want this function to create a pointer that just floats around
-        // Also feel like there is a risk there
-        for(int x=0; x<dimension; ++x){
-            for(int y=0; y<dimension; ++y){
-                field[x*dimension+y] = backupField[x*dimension+y];
-            }
-        }
-        delete(backupField);
+        field = backupField;
     };
 };
 
